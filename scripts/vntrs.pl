b607920730efcd61240050069c7a566b78e25d11
RODRIGUEZ Luis-M <Luismiguel.Rodriguez@ird.fr>
2011-04-11 18:57:22 +0200

    Example script for VNTRs started
    This is supposed to replace (or serve as base to) messy scripts
    using the library (for example the base script for the VNTRs
    detection tool at http://bioinfo-prod.mpl.ird.fr/xantho).

---
+++ scripts/vntrs.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use strict;
+use Polloc::RuleIO;
+use Bio::SeqIO;
+
+
+
+__END__
+
+=pod
+
+=head1 AUTHOR
+
+Luis M. Rodriguez-R < lmrodriguezr at gmail dot com >
+
+=head1 LICENSE
+
+This script is distributed under the terms of
+I<The Artistic License>.  See LICENSE.txt for details.
+
+=head1 SYNOPSIS
+
+    perl polloc_vntrs.pl
+
+=cut
